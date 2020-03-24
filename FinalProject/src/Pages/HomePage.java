98040e470c4a39d0913bdec449e55a1d8eb4abb9
drdenke <nikoladenkedenic@gmail.com>
2020-03-24 22:52:37 +0100

    HomePage and AccessStoreTest

--- FinalProject/src/Pages/HomePage.java
+++ FinalProject/src/Pages/HomePage.java
@@ -1,6 +1,46 @@
 package Pages;
 
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class HomePage {
+	
+	private WebDriver driver;
+	private Properties locators;
+	private WebDriverWait waiter;
+	
+	public HomePage(WebDriver driver,Properties locators, WebDriverWait waiter) {
+		this.driver=driver;
+		this.locators=locators;
+		this.waiter=waiter;
+	}
+	
+	public WebElement getEnter() {
+		return this.driver.findElement(By.xpath(this.locators.getProperty("enter_url")));
+	}
+	
+	public void enterPage() {
+		this.getEnter().click();
+	}
+	
+	public WebElement getMainImage() {
+		return this.driver.findElement(By.xpath(this.locators.getProperty("main_image")));
+	}
+	
+	public boolean mainImageDisplayed() {
+		boolean displayed = false;
+		if(this.getMainImage().isDisplayed()) {
+			displayed = true;
+		}
+		return displayed;
+	}
+	
+	
+	
 	
 	
 }
--- FinalProject/src/Tests/AccessStoreTest.java
+++ FinalProject/src/Tests/AccessStoreTest.java
@@ -1,5 +1,52 @@
 package Tests;
 
-public class AccessStoreTest {
+import static org.testng.Assert.assertTrue;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Pages.HomePage;
+
+public class AccessStoreTest {
+	private WebDriver driver;
+	private Properties locators;
+	private WebDriverWait waiter;
+
+	@BeforeClass
+	public void setup() throws FileNotFoundException, IOException {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.locators = new Properties();
+		locators.load(new FileInputStream("config/home_page.properties"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void accessTest() throws Exception {
+		this.driver.navigate().to(this.locators.getProperty("petstore_url"));
+		
+        HomePage homePage = new HomePage(driver, locators, waiter);
+        homePage.enterPage();
+        
+        assertTrue(homePage.mainImageDisplayed());
+		
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		this.driver.close();
+	}
+	
 }
