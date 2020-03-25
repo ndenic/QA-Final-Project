2815e6bb50b2a47a40e8d22fe5a487b55cde14b6
drdenke <nikoladenkedenic@gmail.com>
2020-03-25 22:01:57 +0100

    RegistrationPage and user.properties commit

--- FinalProject/src/Pages/RegistrationPage.java
+++ FinalProject/src/Pages/RegistrationPage.java
@@ -1,5 +1,53 @@
 package Pages;
 
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class RegistrationPage {
+	private WebDriver driver;
+	private Properties locators;
+	private WebDriverWait waiter;
+//	private JavascriptExecutor jse;
+
+	public RegistrationPage(WebDriver driver, Properties locators, WebDriverWait waiter) {
+		this.driver = driver;
+		this.locators = locators;
+		this.waiter = waiter;
+	}
+	
+	//Getter for Sign In
+	public WebElement getSignIn() {
+		return this.driver.findElement(By.xpath(this.locators.getProperty("signin")));
+	}
+	
+	public void clickOnSignIn() {
+		this.getSignIn().click();
+	}
+	
+	//Getter for Registration
+	public WebElement getRegistratio() {
+		return this.driver.findElement(By.xpath(this.locators.getProperty("register")));
+	}
+	
+	public void clickOnRegisterNow() {
+		this.getRegistratio().click();
+	}
+	
+	public void goToRegisterForm() {
+		clickOnSignIn();
+		clickOnRegisterNow();
+	}
+	
+	
+	
+    
+	
+	
+	
 
 }
