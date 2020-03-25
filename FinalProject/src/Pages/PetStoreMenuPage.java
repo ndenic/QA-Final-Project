9e3ae034cafcadfa0f9b869e8414baa2de17814e
drdenke <nikoladenkedenic@gmail.com>
2020-03-25 14:05:14 +0100

    PetStoreMenuPage and PetStoreMenuTest commit

--- FinalProject/src/Pages/PetStoreMenuPage.java
+++ FinalProject/src/Pages/PetStoreMenuPage.java
@@ -24,10 +24,12 @@ public class PetStoreMenuPage {
 		this.waiter = waiter;
 	}
 
+	//Getter for left side navigation
 	public List<WebElement> getLeftSideNav() {
 		return this.driver.findElements(By.xpath(this.locators.getProperty("left_side_nav")));
 	}
 
+	//Click on one of navigation item
 	public void clickOnLeftNavItem(String name) {
 		List<WebElement> items = this.getLeftSideNav();
 		jse = (JavascriptExecutor) driver;
@@ -41,11 +43,111 @@ public class PetStoreMenuPage {
 		}
 	}
 	
+	//Getter for menu contnent
+    public List<WebElement> getMenuContent(){
+    	return this.driver.findElements(By.xpath(this.locators.getProperty("menu_content")));
+    }
+    
+    //Check validation of menu content links
+	public boolean checkMenuContentLinks() {
+		List<WebElement> links = this.getMenuContent();
+		boolean isOkay = false;
+		for (int i = 0; i < links.size(); i++) {
+			String link = links.get(i).getAttribute("href");
+			if(this.verifyURLStatus(link)<400) {
+				isOkay = true;
+			}
+		}
+		return isOkay;
+	}
 	
+	//Getter for quick links
+	public List<WebElement> getQuickLinks() {
+		return this.driver.findElements(By.xpath(this.locators.getProperty("quick_links")));
+	}
 	
-	
-	
+	//Check validation of quick menu links 
+    public boolean checkQucikMenuLinks() {
+    	List<WebElement> links = this.getQuickLinks();
+    	boolean isOkay = false;
+    	for (int i = 0; i < links.size(); i++) {
+			String link = links.get(i).getAttribute("href");
+			if(this.verifyURLStatus(link)<400) {
+				isOkay = true;
+			}
+		}
+    	return isOkay;
+    }
+    
+    //Getters for Cart
+    public WebElement getCart() {
+    	return this.driver.findElement(By.xpath(this.locators.getProperty("cart")));
+    }
+    public WebElement getCartMessage() {
+    	return this.driver.findElement(By.xpath(this.locators.getProperty("shopping_cart")));
+    }
+    public WebElement getBackFromCart() {
+    	return this.driver.findElement(By.xpath(this.locators.getProperty("get_back_cart")));
+    }
+    //Check cart
+    public boolean checkCart() {
+    	boolean isDisplayed = false;
+    	this.getCart().click();
+    	if(this.getCartMessage().isDisplayed()) {
+    		isDisplayed = true;
+    	}
+    	this.getBackFromCart().click();
+    	return isDisplayed;	
+    }
+    
+    //Getters for Sign In
+    public WebElement getSignIn() {
+    	return this.driver.findElement(By.xpath(this.locators.getProperty("sign_in")));
+    }
+    public WebElement getLoginButton() {
+    	return this.driver.findElement(By.xpath(this.locators.getProperty("login_button")));
+    }
+    public WebElement getJPetStore() {
+    	return this.driver.findElement(By.xpath(this.locators.getProperty("jpet_store")));
+    }
+    //Check Sign in
+    public boolean checkSignIn() {
+    	boolean isDisplayed = false;
+    	this.getSignIn().click();
+    	if(this.getLoginButton().isDisplayed()) {
+    		isDisplayed = true;   		
+    	}
+    	this.getJPetStore().click();
+    	return isDisplayed;
+    	
+    }
+    
+    //Getters for Help link
+    public WebElement getHelp() {
+    	return this.driver.findElement(By.xpath(this.locators.getProperty("help")));
+    }
+    public WebElement getHelpHeader() {
+    	return this.driver.findElement(By.xpath(this.locators.getProperty("help_header")));
+    }
+    //Check Help
+    public boolean checkHelp() {
+    	boolean isDisplayed = false;
+    	this.getHelp().click();
+    	if(this.getHelpHeader().isDisplayed()) {
+    		isDisplayed = true;
+    	}
+    	this.driver.navigate().back();
+    	return isDisplayed;
+    }
+    
+    
+    
+    
+    
 
+	
+	
+	//Method for checking URL validation
 	public int verifyURLStatus(String urlString) {
 		int status = 404;
 		try {
--- FinalProject/src/Tests/PetStoreMenuTest.java
+++ FinalProject/src/Tests/PetStoreMenuTest.java
@@ -3,6 +3,7 @@ package Tests;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Driver;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -12,6 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import Pages.HomePage;
 import Pages.PetStoreMenuPage;
@@ -20,6 +22,10 @@ public class PetStoreMenuTest {
 	private WebDriver driver;
 	private Properties locators;
 	private WebDriverWait waiter;
+
+	public void navigateToPage(String locator) {
+         this.driver.navigate().to(this.locators.getProperty(locator));
+	}
 
 	@BeforeClass
 	public void setup() throws FileNotFoundException, IOException {
@@ -31,25 +37,63 @@ public class PetStoreMenuTest {
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
+
+	@Test(priority = 1)
+	public void linksTest() throws Exception {
+		this.navigateToPage("petstore_url");
+
+		SoftAssert sa = new SoftAssert();
+
+		HomePage homePage = new HomePage(driver, locators, waiter);
+		homePage.enterPage();
+
+		PetStoreMenuPage petStore = new PetStoreMenuPage(driver, locators, waiter);
+
+		petStore.checkMenuContentLinks();
+		petStore.checkQucikMenuLinks();
+
+		sa.assertAll();
+
+	}
 	
-	@Test
-	public void leftSideNavTest() throws Exception{
-		this.driver.navigate().to(this.locators.getProperty("petstore_url"));
+	@Test(priority = 2)
+	public void menuContentTest() throws Exception{
+		this.navigateToPage("petstore_url");
+		
+		SoftAssert sa = new SoftAssert();
 		
 		HomePage homePage = new HomePage(driver, locators, waiter);
 		homePage.enterPage();
 		
+		PetStoreMenuPage petStore = new PetStoreMenuPage(driver, locators, waiter);
+		petStore.checkCart();
+		petStore.checkSignIn();
+		petStore.checkHelp();
+		
+		sa.assertAll();
+		
+		
+	}
+
+
+	@Test(priority = 3)
+	public void leftSideNavTest() throws Exception {
+		this.navigateToPage("petstore_url");
+		
+		HomePage homePage = new HomePage(driver, locators, waiter);
+		homePage.enterPage();
+
 		String name = "reptiles";
-		
-		PetStoreMenuPage petstore = new PetStoreMenuPage(driver, locators, waiter);
-		petstore.clickOnLeftNavItem(name);
-		
+
+		PetStoreMenuPage petStore = new PetStoreMenuPage(driver, locators, waiter);
+		petStore.clickOnLeftNavItem(name);
+
 		Thread.sleep(3000);
 	}
-	
 
 	@AfterClass
 	public void afterClass() {
 		this.driver.close();
 	}
+
 }
