57f528aeaa3387ccfa625c946d8054df30c87178
drdenke <nikoladenkedenic@gmail.com>
2020-03-26 16:37:18 +0100

    RegistrationPage , Test and properties

--- FinalProject/src/Pages/RegistrationPage.java
+++ FinalProject/src/Pages/RegistrationPage.java
@@ -14,99 +14,265 @@ public class RegistrationPage {
 	private Properties locators;
 	private WebDriverWait waiter;
 //	private JavascriptExecutor jse;
+	private final String path = "data/pet-store-data.xlsx";
 
 	public RegistrationPage(WebDriver driver, Properties locators, WebDriverWait waiter) {
 		this.driver = driver;
 		this.locators = locators;
 		this.waiter = waiter;
 	}
-	
-	//Getter for Sign In
+
+	// Getter for Sign In
 	public WebElement getSignIn() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("signin")));
 	}
-	
+
 	public void clickOnSignIn() {
 		this.getSignIn().click();
 	}
-	
-	//Getter for Registration
+
+	// Getter for Registration
 	public WebElement getRegistratio() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("register")));
 	}
-	
+
 	public void clickOnRegisterNow() {
 		this.getRegistratio().click();
 	}
-	
-	public void goToRegisterForm() {
+
+	public void goToRegistrationPage() {
 		clickOnSignIn();
 		clickOnRegisterNow();
 	}
-	
-	//Getters
+
+	// UserID
 	public WebElement getUserId() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("user_id")));
 	}
+
+	public void setUserId(String ID) {
+		this.getUserId().clear();
+		this.getUserId().sendKeys(ID);
+	}
+
+	// New password
 	public WebElement getNewPassword() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("new_password")));
 	}
+
+	public void setNewPassword(String newPassword) {
+		this.getNewPassword().clear();
+		this.getNewPassword().sendKeys(newPassword);
+	}
+
+	// Repeat password
 	public WebElement getRepeatPassword() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("repeat_password")));
 	}
+
+	public void setRepeatPassword(String repeatPassword) {
+		this.getRepeatPassword().clear();
+		this.getRepeatPassword().sendKeys(repeatPassword);
+	}
+
+	// First name
 	public WebElement getFirstName() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("first_name")));
 	}
+
+	public void setFirstName(String firstName) {
+		this.getFirstName().clear();
+		this.getFirstName().sendKeys(firstName);
+	}
+
+	// Last name
 	public WebElement getLastName() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("last_name")));
 	}
+
+	public void setLastName(String lastName) {
+		this.getLastName().clear();
+		this.getLastName().sendKeys(lastName);
+	}
+	
+	//Email
+	public WebElement getEmail() {
+		return this.driver.findElement(By.xpath(this.locators.getProperty("email")));
+	}
+	
+	public void setEmail(String email) {
+		this.getEmail().clear();
+		this.getEmail().sendKeys(email);
+	}
+
+	// Phone
 	public WebElement getPhone() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("phone")));
 	}
+
+	public void setPhone(String phone) {
+		this.getPhone().clear();
+		this.getPhone().sendKeys(phone);
+	}
+
+	// Address 1
 	public WebElement getAddress1() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("address_1")));
 	}
+
+	public void setAddress1(String address1) {
+		this.getAddress1().clear();
+		this.getAddress1().sendKeys(address1);
+	}
+
+	// Addres 2
 	public WebElement getAddress2() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("address_2")));
 	}
+
+	public void setAddress2(String address2) {
+		this.getAddress2().clear();
+		this.getAddress2().sendKeys(address2);
+	}
+
+	// City
 	public WebElement getCity() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("city")));
 	}
+
+	public void setCity(String city) {
+		this.getCity().clear();
+		this.getCity().sendKeys(city);
+	}
+
+	// State
 	public WebElement getState() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("state")));
 	}
+
+	public void setState(String state) {
+		this.getState().clear();
+		this.getState().sendKeys(state);
+	}
+
+	// Zip
 	public WebElement getZip() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("zip")));
 	}
+
+	public void setZip(String zip) {
+		this.getZip().clear();
+		this.getZip().sendKeys(zip);
+	}
+
+	// Country
 	public WebElement getCounty() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("country")));
 	}
-	
-	//Select for language and favorite
+
+	public void setCountry(String country) {
+		this.getCounty().clear();
+		this.getCounty().sendKeys(country);
+	}
+
+	// Select for language
 	public WebElement getLanguage() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("language")));
 	}
-    public Select getLanguageSelect() {
-    	return new Select(this.getLanguage());
-    }
-    public WebElement getFavorite() {
+	public Select getLanguageSelect() {
+		return new Select(this.getLanguage());
+	}
+	public void setLanguageByValue(String value) {
+		this.getLanguageSelect().selectByValue(value);
+	}
+	public void setLanguageById(int id) {
+		this.getLanguageSelect().selectByIndex(id);
+	}
+
+	
+	//Select for favorite
+	public WebElement getFavorite() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("favorite")));
 	}
-    public Select getFavoriteSelect() {
-    	return new Select(this.getFavorite());
-    }
-    
-    
-    public void fillFields() {
-    	
-    }
+	public Select getFavoriteSelect() {
+		return new Select(this.getFavorite());
+	}
+	public void setFavoriteByValue(String value) {
+		this.getFavoriteSelect().selectByValue(value);
+	}
+	public void setFavoriteById(int id) {
+		this.getFavoriteSelect().selectByIndex(id);
+	}
 	
+	//My list
+	public WebElement getMyListButton() {
+		return this.driver.findElement(By.xpath(this.locators.getProperty("my_list")));
+	}
+	public void clickOnMyListButton() {
+		this.getMyListButton().click();
+	}
 	
+	//My banner
+	public WebElement getMyBannerButton() {
+		return this.driver.findElement(By.xpath(this.locators.getProperty("my_banner")));
+	}
+	public void clickOnMyBannerButton() {
+		this.getMyBannerButton().click();
+	}
 	
+	//Save button
+	public WebElement getSaveButton() {
+		return this.driver.findElement(By.xpath(this.locators.getProperty("save")));
+	}
+	public void saveInformations() {
+		this.getSaveButton().click();
+	}
 	
-    
+	//Exception message - 505
+	public WebElement getExceptionMsg() {
+		//this message is only received if all required fields are not filled
+		return this.driver.findElement(By.xpath(this.locators.getProperty("exception_msg")));
+	}
 	
-	
-	
+	//Home page image
+	public WebElement getHomePageImg() {
+		return this.driver.findElement(By.xpath(this.locators.getProperty("homepage_img")));
+	}
 
+	// Registration 
+	public void fillRegistration(int ID) {
+		ExcelUtils excel = new ExcelUtils();
+		excel.setExcell(path);
+		excel.setWorkSheet(1);
+		
+		this.setUserId(excel.getDataAt(ID, 0));
+		this.setNewPassword(excel.getDataAt(ID, 1));
+		this.setRepeatPassword(excel.getDataAt(ID, 1));
+		this.setFirstName(excel.getDataAt(ID, 2));
+		this.setLastName(excel.getDataAt(ID, 3));
+		this.setEmail(excel.getDataAt(ID, 4));
+		this.setPhone(excel.getDataAt(ID, 5));
+		this.setAddress1(excel.getDataAt(ID, 6));
+		this.setAddress2(excel.getDataAt(ID, 7));
+		this.setCity(excel.getDataAt(ID, 8));
+		this.setState(excel.getDataAt(ID, 9));
+		this.setZip(excel.getDataAt(ID, 10));
+	    this.setCountry(excel.getDataAt(ID, 11));
+	}
+	
+	//Check if registration is successfully saved
+	public boolean checkRegistraion() {
+		boolean saved = false;	
+		try {	
+		if(this.getHomePageImg().isDisplayed()){
+		     saved = true;
+		    }
+		}catch(Exception e) {
+			System.out.println("No such element!");
+		}
+		return saved;	
+	}
+	
+	
+	
 }
--- FinalProject/src/Tests/RegistrationTest.java
+++ FinalProject/src/Tests/RegistrationTest.java
@@ -1,5 +1,71 @@
 package Tests;
 
-public class RegistrationTest {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Pages.HomePage;
+import Pages.RegistrationPage;
+
+public class RegistrationTest {
+	private WebDriver driver;
+	private Properties locators;
+	private WebDriverWait waiter;
+
+	public void navigateToPage(String locator) {
+         this.driver.navigate().to(this.locators.getProperty(locator));
+	}
+
+	@BeforeClass
+	public void setup() throws FileNotFoundException, IOException {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.locators = new Properties();
+		locators.load(new FileInputStream("config/user.properties"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void registrationTest() throws Exception {
+		this.navigateToPage("petstore_url");
+		
+		HomePage homePage = new HomePage(driver, locators, waiter);
+		homePage.enterPage();
+		
+		RegistrationPage registration = new RegistrationPage(driver, locators, waiter);
+		
+		int ID = 3;
+		
+		registration.goToRegistrationPage();
+		
+		registration.fillRegistration(7);
+        registration.setLanguageByValue("japanese");
+        registration.setFavoriteById(1);
+        registration.clickOnMyListButton();
+        registration.clickOnMyBannerButton();
+        registration.saveInformations();
+        
+        
+        Assert.assertTrue(registration.checkRegistraion());
+        
+        
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		this.driver.close();
+	}
 }
