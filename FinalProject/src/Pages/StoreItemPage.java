a279c194d4d48f3cad433e96908de99307930099
drdenke <nikoladenkedenic@gmail.com>
2020-03-28 18:46:59 +0100

    StoreItemPage and properties commit

--- FinalProject/src/Pages/StoreItemPage.java
+++ FinalProject/src/Pages/StoreItemPage.java
@@ -1,21 +1,34 @@
 package Pages;
 
+import java.util.List;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class StoreItemPage {
 	private WebDriver driver;
 	private Properties locators;
 	private WebDriverWait waiter;
-	private final String path = "data/pet-store-data.xlsx";
 
 	public StoreItemPage(WebDriver driver, Properties locators, WebDriverWait waiter) {
 		this.driver = driver;
 		this.locators = locators;
 		this.waiter = waiter;
 	}
+	
+	// Quick links
+	public WebElement getAddToCart(){
+		return this.driver.findElement(By.xpath(this.locators.getProperty("add_to_cart")));
+	}
+	
+	// Click on button
+	public void clickOnAddToCart() {
+		this.getAddToCart().click();
+	}
+	
 	
 	
 }
